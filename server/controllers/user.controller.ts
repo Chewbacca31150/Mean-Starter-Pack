9e2dae4e34e3442852e53212cffc70fdec52d095
Chewie <chewbacca31150@gmail.com>
2018-02-13 00:24:56 +0100

    removing useless files

--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -4,17 +4,16 @@ import { Controller, Get, Post } from '@decorators/express';
 import { Inject } from "@decorators/di/src/decorators/inject";
 import { UserRepository } from "../repository/user.repository";
 import { NextFunction } from "express-serve-static-core";
-import { PasswordHandler } from "../utils/password-handler";
 import { IUser } from "../models/user.interface";
 
 @Controller('/users')
 export class UserController {
-  constructor(@Inject(UserRepository) public UserRepository: UserRepository) { }
+  constructor( @Inject(UserRepository) public UserRepository: UserRepository) { }
 
   @Get('/')
   getUsers(request: Request, response: Response): Promise<Response> {
     return this.UserRepository.find()
-      .then(users => {
+      .then((users: IUser[]) => {
         return response.json({ data: users });
       });
   }
